a47fe2a6b010e14fa5fe7c4dacebd910cfe5b0c6
Yang <yangruoyu000@live.com>
2022-03-31 23:04:57 +0800

    fix a problem

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ logging.getLogger('').addHandler(file_log_handler)
 if c.getkey("telegram_bot_server") != "" and c.getkey("telegram_bot_server") is not None:
     from telebot import apihelper
     apihelper.API_URL = c.getkey("telegram_bot_server")
+    fileurl = c.getkey("telegram_bot_server_file")
 else:
-    c.setkey("telegram_bot_server", "https://api.telegram.org/file/bot{0}/{1}")
+    c.setkey("telegram_bot_server", "https://api.telegram.org/bot{0}/{1}")
+    fileurl = "https://api.telegram.org/file/bot{0}".format(c.getkey("telegram_api_token"))
     #c.reload()
 bot = telebot.TeleBot(c.getkey("telegram_api_token"))
 def genuuid():
@@ -37,7 +39,7 @@ def handle_docs(message):
         return
     file_info = bot.get_file(getattr(message,stype).file_id)
     #file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(c.getkey("telegram_api_token"), file_info.file_path))
-    dl_url = c.getkey("telegram_bot_server").format(c.getkey("telegram_api_token"), file_info.file_path)
+    dl_url =  fileurl +"/"+ file_info.file_path
     nuuid = genuuid() + ".ibin"
     with requests.get(dl_url, stream=True,timeout=c.getkey("timeout")) as req:
         with open(os.path.join(c.getkey("tmp_path"),nuuid), 'wb') as f:
@@ -68,7 +70,7 @@ def handle_photo(message):
         return
     file_info = bot.get_file(message.photo[-1].file_id)
     #file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(c.getkey("telegram_api_token"), file_info.file_path))
-    dl_url = c.getkey("telegram_bot_server").format(c.getkey("telegram_api_token"), file_info.file_path)
+    dl_url = fileurl +"/"+ file_info.file_path
     nuuid = genuuid() + ".ibin"
     with requests.get(dl_url, stream=True,timeout=c.getkey("timeout")) as req:
         with open(os.path.join(c.getkey("tmp_path"),nuuid), 'wb') as f:
