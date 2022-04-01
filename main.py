ce1af6c15983d68551619bb660f1102e4d83ed45
Yang <yangruoyu000@live.com>
2022-04-01 14:33:10 +0800

    merge codes

--- main.py
+++ main.py
@@ -31,51 +31,21 @@ def genuuid():
     okey = str(ouid)
     return okey
 
-@bot.message_handler(content_types=['document','audio','video'])
+@bot.message_handler(content_types=['document','audio','video','photo'])
 def handle_docs(message):
     stype =message.content_type
-    file_size = getattr(message,stype).file_size
-    if file_size > c.getkey("maxium_size"):
-        bot.reply_to(message, "File size is too big")
-        return
-    file_info = bot.get_file(getattr(message,stype).file_id)
-    if c.getkey("telegram_bot_server") != "" and c.getkey("telegram_bot_server") is not None and c.getkey("telegram_bot_server") != "https://api.telegram.org/bot{0}/{1}":
-        newfile = file_info.file_path
-    else:
-        #file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(c.getkey("telegram_api_token"), file_info.file_path))
-        dl_url =  fileurl +"/"+ file_info.file_path
-        nuuid = genuuid() + ".ibin"
-        newfile = os.path.join(c.getkey("tmp_path"),nuuid)
-        with requests.get(dl_url, stream=True,timeout=c.getkey("timeout")) as req:
-            with open(os.path.join(c.getkey("tmp_path"),nuuid), 'wb') as f:
-                for chunk in req.iter_content(chunk_size=c.getkey("chunk_size")):
-                    if chunk:
-                        f.write(chunk)
-    
-    #params = urllib.parse.urlencode({"pin":True,"path":dl_url}, quote_via=urllib.parse.quote)
-    ret = requests.post(c.getkey("ipfs_api_host")+"/api/v0/add?pin=true", files={'file': open(os.path.join(newfile), 'rb')},timeout=c.getkey("timeout"))
-    if ret.status_code != 200:
-        bot.reply_to(message, "Error: "+ret.text)
-        return
-    else:
-        ret = ret.json()
-        if "Hash" in ret:
-            bot.reply_to(message, "Your CID is `"+ret["Hash"]+"`\nUse @vsharecloud_bot to keep it permanently\n使用 @vsharecloud_bot 以永久保存文件。",parse_mode="Markdown")
-        else:
-            bot.reply_to(message, "Error: "+ret)
-    if not (c.getkey("telegram_bot_server") != "" and c.getkey("telegram_bot_server") is not None and c.getkey("telegram_bot_server") != "https://api.telegram.org/bot{0}/{1}"):
-        try:
-            os.remove(os.path.join(c.getkey("tmp_path"),nuuid))
-        except FileNotFoundError:
-            pass
-
-@bot.message_handler(content_types=['photo'])
-def handle_photo(message):
-    file_size = message.photo[-1].file_size
-    if file_size > c.getkey("maxium_size"):
-        bot.reply_to(message, "File size is too big")
-        return
-    file_info = bot.get_file(message.photo[-1].file_id)
+    try:
+        file_size = getattr(message,stype).file_size
+        if file_size > c.getkey("maxium_size"):
+            bot.reply_to(message, "File size is too big")
+            return
+        file_info = bot.get_file(getattr(message,stype).file_id)
+    except:
+        file_size = getattr(message,stype)[-1].file_size
+        if file_size > c.getkey("maxium_size"):
+            bot.reply_to(message, "File size is too big")
+            return
+        file_info = bot.get_file(getattr(message,stype)[-1].file_id)
     if c.getkey("telegram_bot_server") != "" and c.getkey("telegram_bot_server") is not None and c.getkey("telegram_bot_server") != "https://api.telegram.org/bot{0}/{1}":
         newfile = file_info.file_path
     else:
